1f36a8cc5096463520c9a7bbabd8078bad944508
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2020-10-04 17:37:04 -0700

    Added edit/update functionality and flash notices and errors for sucessful or unsucessful saves/updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,38 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+    render :index
+  end
+
   def new
     @user = User.new
-    
+    render :new
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to new_user_url
+      flash.notice = "#{@user.username} successfully saved!"
+      redirect_to root_url
     else
+      flash.now[:errors] = @user.errors.full_messages
       render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash.notice = "#{@user.username} successfully updated!"
+      redirect_to root_url
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :edit
     end
   end
 
